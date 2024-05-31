e68562d446bd290c720ac8219e3bc63eceef9354
JustinHinton <justgawd7@icloud.com>
2024-05-31 16:47:43 -0600

    added git for indexed db files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,6 +16,15 @@ const initdb = async () =>
 export const putDb = async (content) => console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'jate');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value
+
+}
 
 initdb();
